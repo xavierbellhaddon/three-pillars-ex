1f0bd3e09c8da7025d9070ef54ed5bb4b6f2f296
xavierbellhaddon <59543361+xavierbellhaddon@users.noreply.github.com>
2020-01-14 19:38:57 -0800

    first commit

---
+++ ex.js
@@ -0,0 +1,41 @@
+class Bookshelf {
+	constructor() {
+		this.favoriteBooks = [];
+  }
+		addFavoriteBook(bookName) {
+			if (!bookName.includes("Great")) {
+				this.favoriteBooks.push(bookName);
+			}
+		}
+		printFavoriteBooks() {
+			console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+			for (let bookName of this.favoriteBooks) {
+				console.log(bookName);
+			}
+	 }
+}
+
+function loadBooks(bookshelf) {
+  fakeAjax(BOOK_API, function onBooks(bookNames) {
+    for (let bookName of bookNames) {
+      bookshelf.addFavoriteBook(bookName);
+    }
+    bookshelf.printFavoriteBooks();
+  });
+}
+
+let BOOK_API = "https://some.url/api";
+let myBooks = new Bookshelf();
+loadBooks(myBooks);
+
+function fakeAjax(url, cb) {
+	setTimeout(function fakeLoadingDelay() {
+		cb([
+			"A Song of Ice and Fire",
+			"The Great Gatsby",
+			"Crime & Punishment",
+			"Great Expectations",
+			"You Don't Know JS"
+		]);
+	}, 500);
+}
